c2075b5a8b319629a3565a1f7e289635b3302501
kennja05 <jacobkenny05@gmail.com>
2020-10-29 18:02:08 -0400

    use canvas to draw image

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
-    document.addEventListener('mousemove', function(e){
-        console.log(e.x, e)
-    })
+    draw()
 })
+
+function draw(){
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+    img.onload = function(){
+        ctx.drawImage(img, 0, 0, 500, 300)
+    }
+    img.src = 'cats.jpg'
+    console.log(canvas.width, canvas.height)
+}
