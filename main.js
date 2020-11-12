78415a9cbebcb223a9991a9fbc3455e8d6076a7a
kennja05 <jacobkenny05@gmail.com>
2020-11-12 15:56:51 -0500

    clean up main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
     draw()
     let circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
-    circle.setAttribute('fill', '#00000000')
+    circle.setAttribute('fill', 'rgba(0, 0, 0, 0.5')
     circle.setAttribute('stroke', '#000')
     circle.setAttribute('r', '10px')
-    circle.setAttribute('cx', '11px')
-    circle.setAttribute('cy', '11px')
+    circle.setAttribute('cx', '25px')
+    circle.setAttribute('cy', '25px')
     document.getElementById('svg').appendChild(circle);
     document.addEventListener('mousemove', onMouseMove)
 })
@@ -21,17 +21,8 @@ function draw(){
     console.log(canvas.width, canvas.height)
 }
 
-function mouseX(event){
-    return event.mouseX
-}
-
-function mouseY(event){
-    return event.mouseY
-}
-
-let circle = document.getElementById('svg');
-
-const onMouseMove = (e) =>{
-  circle.style.left = e.pageX + 'px';
-  circle.style.top = e.pageY + 'px';
+function onMouseMove(e) {
+    const circle = document.getElementById('svg');
+    circle.style.left = e.pageX + 'px';
+    circle.style.top = e.pageY + 'px';
 }
